8feeecc02d0e7d3796c21a733bd6e24f4f2755b3
badman4k-png <6306021622118@fitm.kmutnb.ac.th>
2022-03-30 14:34:37 +0700

    L3-L4

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            Url: 'https://www.vuemastery.com/',
+            inventory: 100,
+            onSale: true
         }
     }
 })
